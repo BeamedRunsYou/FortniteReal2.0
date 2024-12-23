a038257083fd4ca59a0ed0d9064f33c7cc1daa4a
BeamedRunsYou <bluestripz963@gmail.com>
2024-12-22 18:52:43 -0800

    Create styles.cc

---
+++ styles.cc
@@ -0,0 +1,37 @@
+body, html {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: 'Bangers', cursive;
+    background-image: url('https://wallpaperaccess.com/full/2102764.jpg');
+    background-size: cover;
+    background-position: center;
+}
+
+.container {
+    text-align: center;
+    color: white;
+}
+
+.title {
+    font-size: 3em;
+    margin-bottom: 20px;
+}
+
+.welcome-button {
+    font-size: 1.5em;
+    padding: 10px 20px;
+    color: white;
+    background-color: #ff4757;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.3s;
+}
+
+.welcome-button:hover {
+    background-color: #ff6b81;
+}
